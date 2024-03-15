6aaa70db775abff0e6671c8573ff4514a735522b
incognito <inc3gnito@gmail.com>
2024-03-15 19:49:47 +0100

    version 2

--- legnagyobb szam.py
+++ legnagyobb szam.py
@@ -1,6 +1,6 @@
-elso_szam = int(input('Kerem az elso szamot!'))
-masodik_szam = int(input('Kerem a masodik szamot!'))
-harmadik_szam = int(input('Kerem a harmadik szamot!'))
+elso_szam = int(input('Kerem az elso szamot!' ))
+masodik_szam = int(input('Kerem a masodik szamot!' ))
+harmadik_szam = int(input('Kerem a harmadik szamot!'  ))
 print (f'A megadott szamok: {elso_szam}, {masodik_szam}, {harmadik_szam}')
 legnagyobb_szam = elso_szam
 if     legnagyobb_szam < masodik_szam:
@@ -8,4 +8,5 @@ if     legnagyobb_szam < masodik_szam:
 if      legnagyobb_szam < harmadik_szam:
     legnagyobb_szam = harmadik_szam
     print(f"legnagyobb_szam: {legnagyobb_szam}")
+    
     
