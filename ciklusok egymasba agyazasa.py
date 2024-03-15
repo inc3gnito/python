b180d894e74ad9cb4fc71119ed39bd0535850cbd
inc3gnito <inc3gnito@gmail.com>
2024-03-15 22:07:03 +0100

    version 1

--- ciklusok egymasba agyazasa.py
+++ ciklusok egymasba agyazasa.py
@@ -1,8 +1,10 @@
 sor = 1
+darab = 1
 while sor <= 4:
     oszlop = 1
-    while oszlop <= 7:
-        print('O ',end='')
-        oszlop = oszlop + 1
-        print('')
-        sor = sor + 1
+    while oszlop <= darab:
+          print('O ',end='')
+          oszlop = oszlop + 1
+    print('')
+    darab = darab +1
+    sor = sor + 1
