44ceafb04a655a0bdfcdd4c100cc1afdd851a6e2
inc3gnito <inc3gnito@gmail.com>
2024-03-17 17:22:57 +0100

    version 1

---
+++ adatbekeres while ciklussal.py
@@ -0,0 +1,7 @@
+szam = int(input('Adj meg egy szamot 5 és 10 között!' ))
+while not 5 <= szam <= 10:
+    szam = int(input('Helytelen ertek! Adj meg egy szamot 5 es 10 között' ))
+
+print('Rendben!')
+    
+    
