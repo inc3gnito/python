3814d34678e30c7ba1ccf6ad455d079d6628ebdd
incognito <inc3gnito@gmail.com>
2024-03-29 11:33:30 +0100

    version 5

--- logikai kifejezések.py
+++ logikai kifejezések.py
@@ -1,11 +1,10 @@
 szam =int(input('Adj meg egy számot!  '))
 if szam < 0:
-    print('A szám negatív' )
+    print('A szám negatív'-)
 if szam == 0:
     print('A szám nulla')
 else:
     print('A szám pozitív')
-    exit()
     
         
     
