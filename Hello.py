a40005a45f3b3f027a4cfd42149b863c3134ddb3
inc3gnito <inc3gnito@gmail.com>
2024-08-17 11:48:25 +0200

    latest

--- Hello.py
+++ Hello.py
@@ -1,6 +1,8 @@
 # Ez az első python programom!
-szam = int(input('Adj meg egy egész számot'))
+szam = int(input('Adj meg egy egész számot' ))
 if szam < 0:
   print ('A megadott szam negatív')
-  print ('A program vége')
+if szam > 0:
+  print ('A megadott szam pozitív')
+  print ('A program vége') 
   
